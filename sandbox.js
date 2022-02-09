cc6144dc1d9efbbc7920b71982de5ff0abfc2ad7
SpicyLunchbox <westonwoodard28@gmail.com>
2022-02-09 13:00:00 -0600

    type testing with all types

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,23 @@
-var character = "luigi";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+// initialize compile and watch with tsc sandbox.ts -w
+// typescript uses inference to determine what type a variable is
+var character = "mario";
+var age = 30;
+var isBlackBelt = false;
+// character = 20 won't work
+character = "luigi";
+// type check is done prior to compile
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
+var names = ["luigi", "mario", "yoshi"];
+names.push("toad");
+// line below won't work
+// names.push(3);
+// arrays can have mixed types if they are initialized with such
+var mixed = ["ken", 4, "chun-li", 8, 9];
+var ninja = {
+    name: "mario",
+    belt: "black",
+    age: 30
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,11 +1,31 @@
-const character = "luigi";
+// initialize compile and watch with tsc sandbox.ts -w
+// typescript uses inference to determine what type a variable is
 
-console.log(character);
+let character = "mario";
+let age = 30;
+let isBlackBelt = false;
 
-const inputs = document.querySelectorAll("input");
+// character = 20 won't work
+character = "luigi";
 
-console.log(inputs);
+// type check is done prior to compile
+const circ = (diameter: number) => {
+	return diameter * Math.PI;
+};
 
-inputs.forEach((input) => {
-	console.log(input);
-});
+console.log(circ(7.5));
+
+let names = ["luigi", "mario", "yoshi"];
+
+names.push("toad");
+// line below won't work
+// names.push(3);
+
+// arrays can have mixed types if they are initialized with such
+let mixed = ["ken", 4, "chun-li", 8, 9];
+
+let ninja = {
+	name: "mario",
+	belt: "black",
+	age: 30,
+};
